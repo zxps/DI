c0311afd65c2ffec0688cf6b5bc8001e979ddf4e
stepan panin <stepan.panin@corp.mail.ru>
2021-05-24 09:53:17 +0300

    replace container names with fqcn

--- closured_container/src/DI/Bundles/Common/Common.php
+++ closured_container/src/DI/Bundles/Common/Common.php
@@ -10,17 +10,6 @@ use Services;
 use Storages;
 
 class Common extends AbstractContainer implements Container {
-  private const ID_MEOW_CLUSTER  = 'meow_cluster';
-  private const ID_CACHE_STORAGE = 'cache_storage';
-
-  private const ID_PRODUCTS_DEFAULT_SERIALIZER = 'products_default_serializer';
-  private const ID_PRODUCTS_CACHE_SERIALIZER   = 'products_cache_serializer';
-  private const ID_PRODUCTS_SERVICE            = 'products_service';
-  private const ID_PRODUCTS_STORAGE            = 'products_storage';
-
-  private const ID_ORDERS_STORAGE = 'orders_storage';
-  private const ID_ORDERS_SERVICE = 'orders_service';
-
   private Configuration $configuration;
 
   public function __construct(array $parameters = []) {
@@ -34,13 +23,13 @@ class Common extends AbstractContainer implements Container {
 
   public function getProductsService(): Services\ProductsService {
     /** @var Services\ProductsService $service */
-    $service = $this->get(self::ID_PRODUCTS_SERVICE);
+    $service = $this->get(Services\ProductsService::class);
     return $service;
   }
 
   public function getOrdersService(): Services\OrdersService {
     /** @var Services\OrdersService $service */
-    $service = $this->get(self::ID_ORDERS_SERVICE);
+    $service = $this->get(Services\OrdersService::class);
     return $service;
   }
 
@@ -48,48 +37,47 @@ class Common extends AbstractContainer implements Container {
    * Manual lazy registration/injecting
    */
   protected function configure(): void {
-    $this->register(self::ID_MEOW_CLUSTER, function(): Cluster\Meow {
+    $this->register(Cluster\Meow::class, function(): Cluster\Meow {
       return new Cluster\Meow($this->getConfiguration()->getParameters()->get(\DI\Bundles\Common\Configuration::KEY_MEOW_ACTOR));
     });
 
-    $this->register(self::ID_PRODUCTS_DEFAULT_SERIALIZER, function(): Serializers\Products\DefaultSerializer {
+    $this->register(Serializers\Orders\DefaultSerializer::class, function(): Serializers\Products\DefaultSerializer {
       return new Serializers\Products\DefaultSerializer();
     });
 
-    $this->register(self::ID_PRODUCTS_CACHE_SERIALIZER, function(): Serializers\Products\CacheSerializer {
+    $this->register(Serializers\Products\CacheSerializer::class, function(): Serializers\Products\CacheSerializer {
       return new Serializers\Products\CacheSerializer();
     });
 
-    $this->register(self::ID_PRODUCTS_STORAGE, function(): Storages\ProductsStorage {
+    $this->register(Storages\ProductsStorage::class, function(): Storages\ProductsStorage {
       /** @var Cluster\Meow $cluster */
-      $cluster = $this->get(self::ID_MEOW_CLUSTER);
+      $cluster = $this->get(Cluster\Meow::class);
 
       /** @var Storages\CacheStorage $cache */
-      $cache = $this->get(self::ID_CACHE_STORAGE);
+      $cache = $this->get(Storages\CacheStorage::class);
 
       /** @var Serializers\Products\DefaultSerializer $engineSerializer */
-      $engineSerializer = $this->get(self::ID_PRODUCTS_DEFAULT_SERIALIZER);
+      $engineSerializer = $this->get(Serializers\Products\DefaultSerializer::class);
 
       /** @var Serializers\Products\CacheSerializer $cacheSerializer */
-      $cacheSerializer = $this->get(self::ID_PRODUCTS_CACHE_SERIALIZER);
+      $cacheSerializer = $this->get(Serializers\Products\CacheSerializer::class);
 
       return new Storages\ProductsStorage($cluster, $cache, $engineSerializer, $cacheSerializer);
     });
 
-    $this->register(self::ID_ORDERS_STORAGE, function(): Storages\OrdersStorage {
+    $this->register(Storages\OrdersStorage::class, function(): Storages\OrdersStorage {
       /** @var Cluster\Meow $cluster */
-      $cluster = $this->get(self::ID_MEOW_CLUSTER);
+      $cluster = $this->get(Cluster\Meow::class);
 
       /** @var Storages\CacheStorage $cache */
-      $cache = $this->get(self::ID_PRODUCTS_CACHE_SERIALIZER);
+      $cache = $this->get(Storages\CacheStorage::class);
       return new Storages\OrdersStorage($cluster, $cache);
     });
 
-    $this->register(self::ID_ORDERS_SERVICE, function(): Services\ProductsService {
-      /** @var Storages\ProductsStorage $storage */
-      $storage = $this->get(self::ID_ORDERS_STORAGE);
-
-      return new Services\ProductsService($storage);
+    $this->register(Services\OrdersService::class, function(): Services\OrdersService {
+      /** @var Storages\OrdersStorage $storage */
+      $storage = $this->get(Storages\OrdersStorage::class);
+      return new Services\OrdersService($storage);
     });
   }
 }
